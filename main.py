8ddc4ad6b7274cd6de5f1a0e5d196dc396269dcd
Thalia153 <alice.nguyn@gmail.com>
2020-10-19 10:39:45 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from time import sleep
+from selenium import webdriver
+browser = webdriver.Chrome('./chromedriver.exe')
+browser.get('https://www.got-it.ai/solutions/excel-chat/')
+login = browser.find_element_by_id('test-login-button')
+login.click()
+browser.implicitly_wait(10)
+email = browser.find_element_by_name('email')
+email.click()
+email.send_keys('alice.nguyn@gmail.com')
+password = browser.find_element_by_name('password')
+password.click()
+password.send_keys('Excelchat153$$$')
+submit = browser.find_element_by_id('login-button')
+submit.click()
+sleep(10)
+browser.close()
